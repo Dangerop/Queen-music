2ed9edb70710516155e2121811a525617d481731
Mahiharshiii <123259771+Mahiharshiii@users.noreply.github.com>
2023-01-21 22:31:16 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Jankarikiduniya/AlexaMusic",
+    "https://github.com/Mahiharshiii/Queen-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
